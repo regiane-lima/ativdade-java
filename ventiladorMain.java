a137c3b1152776ae62ec6147c7be31e2bd00d290
regiane-lima <120677112+regiane-lima@users.noreply.github.com>
2023-06-26 09:01:58 -0300

    Create ventiladorMain.java

---
+++ ventiladorMain.java
@@ -0,0 +1,12 @@
+package calculo;
+
+public class salaDeAula {
+
+    public static void main(String[] args) {
+        ventilador c = new ventilador();
+        c.girar(1);
+        c.mostrarVelocidade();
+
+    }
+
+}
