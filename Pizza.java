a4d8c86f64d832cefae9f60be2ad052cf8177ed0
regiane-lima <120677112+regiane-lima@users.noreply.github.com>
2023-08-03 10:55:24 -0300

    Create Pizza.java
    Main do meu projeto de comer pizza!

---
+++ Pizza.java
@@ -0,0 +1,25 @@
+
+package com.mycompany.pizza;
+
+/**
+ *
+ * @author senai
+ */
+public class Pizza {
+
+    public static void main(String[] args) {
+        ClassPizza p = new ClassPizza();
+        int restoDePizza = 8;
+
+        p.RetiraPedaço();
+        p.RetiraPedaço();
+        p.RetiraPedaço();
+        p.RetiraPedaço();
+        p.RetiraPedaço();
+        p.RetiraPedaço();
+        p.RetiraPedaço();
+        p.RetiraPedaço();
+        p.RetiraPedaço();
+
+    }
+}
